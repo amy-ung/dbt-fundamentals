13f8b04704385506a55685f2d17e7159dd350d26
april.myung@slalom.com <april.myung@slalom.com>
2022-01-25 08:42:51 +0000

    lessons learned

---
+++ DBT Sub/models/staging/stripe/stg_payments.sql
@@ -0,0 +1,13 @@
+with payments as (
+
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        amount/100 as amount,
+        status,
+        created as created_at
+    from {{ source('stripe', 'payment')}}
+
+)
+select * from payments
